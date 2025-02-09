9ebb38a5481b009865d38864a30c0af92d648d8b
VUYO-MODIBZA <110651911+VUYO-MODIBZA@users.noreply.github.com>
2025-02-09 08:02:18 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,10 +6,10 @@ export default function About() {
         <div className="about-page-container">
             <div className="about-page-content">
                 <p>Selina Phetshiwe "Maxaba" Mthimba was the daughter of Nondlala "Xhayiya" Twalo. She was born on May 25, 1908, in the village of Upper Didimana, located in the Hewu district in the Eastern Cape Province of South Africa.</p>
-                <p>She married Chitha Mthimba, and together they were blessed with four children - Tozi Lizzie Ningiza, Stephen Mtimba, Silas Mtimba and Nikiwe Ndidi</p>
+                <p>She married Chitha Mthimba, and together they were blessed with four children - Tozi Lizzie Ningiza, Stephen Mthimba, Silas Mthimba and Nikiwe Ndidi</p>
             </div>
             <div className="about-page-cta">
-            <p>Discover more detailed information about Selina Phetshiwe "Maxaba" Mtimba's family history.</p>
+            <p>Discover more detailed information about Selina Phetshiwe "Maxaba" Mthimba's family history.</p>
             <Link className="link-button" to="/children">Explore the Lineage</Link>
             </div>
         </div>
