516c011226298ff3cda761e03815a51dca2e3ca3
VUYO-MODIBZA <110651911+VUYO-MODIBZA@users.noreply.github.com>
2025-04-03 21:34:51 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,11 +15,11 @@ function Home() {
             <div class="image-container">
             <p>"A family bound together across generations, sharing love and peace at each gathering, celebrating the togetherness that was instilled by those who came before and cherished by those who come after."</p>
             <img className="home-image1" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1731411756/family-men-pic2-2_ewodzj.png" />       
-            <img className="home-image2" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1731412564/family-girls_yoztht.jpg" alt="Family-pic2" />
+            <img className="home-image2" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743702505/Umtshato_ka_Nontombi_e9tess.jpg" alt="Family-pic2" />
             </div>
             <div class="image-container">
             <p>"A family bound together across generations, sharing love and peace at each gathering, celebrating the togetherness that was instilled by those who came before and cherished by those who come after."</p>
-            <img className="home-image1" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743707573/IMG-20211231-WA0033_vcnepb.jpg" />       
+            <img className="home-image1" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1731412564/family-girls_yoztht.jpg" />       
             <img className="home-image2" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743702505/Umtshato_ka_Nontombi_e9tess.jpg" />
             </div>
             <Link to="/">Back to home</Link>
