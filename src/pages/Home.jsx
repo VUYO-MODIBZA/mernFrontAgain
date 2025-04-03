75bc65c00c3e3ad40691ce252c9b27a167034b7c
VUYO-MODIBZA <110651911+VUYO-MODIBZA@users.noreply.github.com>
2025-04-03 20:21:21 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ function Home() {
             </div>
             <div class="image-container">
             <p>"A family bound together across generations, sharing love and peace at each gathering, celebrating the togetherness that was instilled by those who came before and cherished by those who come after."</p>
-            <img className="home-image1" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743702506/Umgidi_ka_Sima_xaliji.jpg" />       
+            <img className="home-image1" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743704346/Umgidi_ka_Sima_pmlz6y.jpg" />       
             <img className="home-image2" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743702505/Umtshato_ka_Nontombi_e9tess.jpg" />
             </div>
             <Link to="/">Back to home</Link>
