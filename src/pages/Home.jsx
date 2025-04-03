3f87c92cc38d6a7ea2cefec4f00935486ffb8c04
VUYO-MODIBZA <110651911+VUYO-MODIBZA@users.noreply.github.com>
2025-04-03 22:13:20 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return(
         <>
         <div>
-            <h1>Welcome to the webpage of Selina Phetshiwe "Maxaba" Mtimba</h1>
+            <h1>Welcome to the webpage of Chitha Mthimba and Selina Phetshiwe "Maxaba" Mthimba</h1>
             <p>Discover the lineage of Selina Phetshiwe "Maxaba" Mtimba, a legacy spanning five generations.</p>
             <p>Explore the stories of her descendants, from her children to her great-great-great-grandchildren.</p>
             <div class="image-container">
@@ -18,10 +18,11 @@ function Home() {
             <img className="home-image2" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743709775/IMG-20211231-WA0033_b1adf1.jpg" />
             </div>
             <div class="image-container">
-            <p>"A family bound together across generations, sharing love and peace at each gathering, celebrating the togetherness that was instilled by those who came before and cherished by those who come after."</p>
+            <p>"Rooted in a rich history, strengthened by tradition, and carried forward with pride, our lineage is a testament to the bonds that time cannot break. "</p>
             <img className="home-image1" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1731412564/family-girls_yoztht.jpg" />       
             <img className="home-image2" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743709775/IMG-20220319-WA0009_u4asgt.jpg" />
             </div>
+            <p>Through stories, memories, and shared experiences, we honor our past, embrace our present, and inspire future generations to continue the legacy.</p>
             <Link to="/">Back to home</Link>
         </div>
         </>
