90bcbee838126d24364064940c6d565c1a80d2e3
VUYO-MODIBZA <110651911+VUYO-MODIBZA@users.noreply.github.com>
2025-04-06 18:56:07 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,12 +5,17 @@ export default function About() {
     return (
         <div className="about-page-container">
             <div className="about-page-content">
-                <p>Chitha Mthimba was the daughter of Nondlala "Xhayiya" Twalo. He was born in ..................., in the village of Upper Didimana, located in the Hewu district in the Eastern Cape Province of South Africa.</p>
+                <p>Chitha Mthimba was the son of Nondlala "Xhayiya" Twalo. He was born in ..................., in the village of Upper Didimana, located in the Hewu district in the Eastern Cape Province of South Africa.</p>
                 <p>He married Selina Phetshiwe Twalo, and together they were blessed with four children - Tozi Lizzie Ningiza, Stephen Mthimba, Silas Mthimba and Nikiwe Ndidi</p>
-            <div className="about-page-cta">
-            <p>Discover more detailed information about Chitha Mthimba's family history.</p>
-            <Link className="link-button" to="/Home">Explore the Lineage</Link>
+                <div className="about-page-cta">
+                    <div className="image-container">
+                        <img className="home-image1" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743956313/Imvelaphi%20ka%20Chitha%20Mthimba.png" alt="Family-tree" />
+                        <p>Discover more detailed information about Chitha Mthimba's family history.</p>
+                        <Link className="link-button" to="/Home">Explore the Lineage</Link>
+                    </div>
+                </div>
             </div>
         </div>
-    );
+    )
 }
+
