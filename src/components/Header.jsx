6deb9879000f3988985c7989ab70467ad6c73240
VUYO-MODIBZA <110651911+VUYO-MODIBZA@users.noreply.github.com>
2025-04-09 20:01:54 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,11 +8,11 @@ export default function Header(){
         <Link to="/">Home</Link>
         <nav>
           <Link to="/about">About</Link>
-          <Link to="/children">My_Children</Link>
-          <Link to="/grandchildren">My_Grandchildren</Link>
-          <Link to="/greatgrandchildren">My_Great-grandchildren</Link>
-          <Link to="/greatgreatgrandchildren">My_Great-great-grandchildren</Link>
-          <Link to="/greatgreatgreatgrandchildren">My_Great-great-great-grandchildren</Link>
+          <Link to="/children">Children</Link>
+          <Link to="/grandchildren">Grandchildren</Link>
+          <Link to="/greatgrandchildren">Great-grandchildren</Link>
+          <Link to="/greatgreatgrandchildren">Great-great-grandchildren</Link>
+          <Link to="/greatgreatgreatgrandchildren">Great-great-great-grandchildren</Link>
         </nav>
     </header>
     )
