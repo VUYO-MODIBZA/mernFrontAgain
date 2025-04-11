44873fa77aaf6c23333e0a79c2cea71a01b846a9
VUYO-MODIBZA <110651911+VUYO-MODIBZA@users.noreply.github.com>
2025-04-11 11:38:53 +0200

    Update Navbar.jsx

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,34 +1,14 @@
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
 
 const Navbar = () => {
-    const [showChildrenMenu, setShowChildrenMenu] = useState(false);
-
-    const toggleChildrenMenu = () => {
-        setShowChildrenMenu(prev => !prev);
-    };
-
     return (
         <nav>
             <NavLink to='/'>Home</NavLink>
             <NavLink to='/about'>About</NavLink>
-            <NavLink to='/children'>My children</NavLink>
-            <div>
-                <span onClick={toggleChildrenMenu} style={{ cursor: 'pointer' }}>
-                    My Children
-                </span>
-                {showChildrenMenu && (
-                    <div className="dropdown-menu">
-                        <NavLink to='/children/tozi'>Tozi_children</NavLink>
-                        <NavLink to='/children/stefano'>Stefano_children</NavLink>
-                        <NavLink to='/children/silas'>Silas_children</NavLink>
-                        <NavLink to='/children/nikiwe'>Nikiwe_children</NavLink>
-                    </div>
-                )}
-            </div>
+            <NavLink to='/children'>My Children</NavLink>
             <NavLink to='/grandchildren'>My Grandchildren</NavLink>
-            <NavLink to='/greatgrandchildren'>My Greatgrandchildren</NavLink>
-            <NavLink to='/greatgreatgrandchildren'>My Greatgreatgrandchildren</NavLink>
+            <NavLink to='/greatgrandchildren'>My Great Grandchildren</NavLink>
+            <NavLink to='/greatgreatgrandchildren'>My Great Great Grandchildren</NavLink>
         </nav>
     );
 };
