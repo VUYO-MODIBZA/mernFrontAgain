5239497a9aab67db83d27d3c71463669a3f896d3
VUYO-MODIBZA <110651911+VUYO-MODIBZA@users.noreply.github.com>
2025-07-27 17:29:43 +0200

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,8 +9,8 @@ export default function About() {
                 <p>He married Selina Phetshiwe Twalo, and together they were blessed with four children - Tozi Lizzie Ningiza, Stephen Mthimba, Silas Mthimba and Nikiwe Ndidi</p>
                 <div className="about-page-cta">
                     <div className="image-container">
-                        <img className="home-image1" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743956313/Imvelaphi%20ka%20Chitha%20Mthimba.png" alt="Family-tree" />
-                        <img className="home-image2" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1744360466/Copy_of_Neutral_Beige_Minimalist_Corporate_Company_Organizational_Chart_d1xvyr.png" />
+                        <img className="home-image1" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1743956313/family-tree/Imvelaphi%20ka%20Chitha%20Mthimba.png" alt="Family-tree" />
+                        <img className="home-image2" src="https://res.cloudinary.com/dhcsne1pn/image/upload/v1744360466/family-tree/Copy_of_Neutral_Beige_Minimalist_Corporate_Company_Organizational_Chart_d1xvyr.png" />
                         <p>Discover more detailed information about Chitha and Phetshiwe Mthimba's family history.</p>
                         <Link className="link-button" to="/children">Explore the Lineage</Link>
                     </div>
